a4a10bd08dab917bf1dd6d1cf98fa77144684459
mineruking <kmin010287@gmail.com>
2024-05-10 13:24:22 +0900

    V1.1 Example test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Stack gap={3}>
+          <div className="p-2">{'Hello'}<Button as="input" type="button" value="Input" /></div>
+          <div className="p-2">Second item</div>
+          <div className="p-2">Third item</div>
+        </Stack>
+      
     </div>
   );
 }
