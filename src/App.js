ccf367bdc8b56081d0c06e4451dfa067f159625d
mineruking <kmin010287@gmail.com>
2024-05-10 13:57:50 +0900

    V1.3 Image Added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
+import wallpaper from './images/wallpaper.jpg'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Stack from 'react-bootstrap/Stack';
 
 function main() {
   return (
-    <>
+    <Stack gap={3}>
+      <div className="p-2"><>
       <Navbar bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home"><img
@@ -28,17 +31,24 @@ function main() {
 
         </Container>
         <Container>
-
-        </Container>
-        <Container>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="Notice">공지사항</Nav.Link>
+            <Nav.Link href="Company">회사 소개</Nav.Link>
+            <Nav.Link href="Product">제품 소개</Nav.Link>
+            <Nav.Link href="SupportCenter">고객 지원</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-    </>
+    </></div>
+      <div className="p-2"><img
+              alt="   "
+              src={wallpaper}
+              width="1920"
+              height="700"
+              className="Wallpaper"
+            /></div>
+      <div className="p-2">Third item</div>
+    </Stack>
   );
 }
 
