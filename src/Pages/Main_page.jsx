34a55226e7c2a643189633001778e9f63d1d8a08
mineruking <kmin010287@gmail.com>
2024-05-14 11:10:15 +0900

    V2.5 - Main Page Edit

--- src/Pages/Main_page.jsx
+++ src/Pages/Main_page.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import wallpaper from '../images/wallpaper.jpg';
 
 const Main = () => {
     const navigate = useNavigate();
     
     return(
         <div>
-            MainPage
+           <img
+              alt=""
+              src={wallpaper}
+              width="1920"
+              height="870"
+              className="d-inline-block align-top"
+              style={{ opacity: 0.5 }} // 이 부분에 투명도를 설정
+            /> 
         </div>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,22 +7,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Menu() {
   return (
-    <Navbar bg="light" data-bs-theme="light">
-    <Container>
-      <Nav.Link href="/"><img
+    <Navbar style={{ backgroundColor: 'transparent', position: 'absolute', top: 0, zIndex: 1 }} variant='light'>
+    <Container className="justify-content-between">
+      <Navbar.Brand href="/"><img
               alt=""
               src={logo}
               width="160"
               height="35"
               className="d-inline-block align-top"
-            />{' '}</Nav.Link>
-    </Container>
-    <Nav className="me-auto">
+            />{' '}</Navbar.Brand>
+      <Nav className="ml-auto">
         <Nav.Link href="notification">공지사항</Nav.Link>
         <Nav.Link href="aboutus">회사 소개</Nav.Link>
         <Nav.Link href="product">제품 소개</Nav.Link>
         <Nav.Link href="support">고객 지원</Nav.Link>
       </Nav>
+    </Container>
   </Navbar>
   );
 }
