be0ad3cac35b056a8ff1d7ae3f883367c8083e30
mineruking <kmin010287@gmail.com>
2024-05-14 11:24:56 +0900

    V2.5.1 Add Text to Main Page

--- src/Pages/Main_page.jsx
+++ src/Pages/Main_page.jsx
@@ -7,15 +7,40 @@ const Main = () => {
     const navigate = useNavigate();
     
     return(
-        <div>
+        <div className="main-image" style={{ position: 'relative' }}>
            <img
               alt=""
               src={wallpaper}
               width="1920"
               height="870"
               className="d-inline-block align-top"
-              style={{ opacity: 0.5 }} // 이 부분에 투명도를 설정
+              style={{ opacity: 0.5 }}
             /> 
+            <div style={{
+              position: 'absolute',
+              top: '35%',
+              left: '20%',
+              transform: 'translate(-50%, -50%)',
+              color: '#0C5A89',
+              fontSize: '60px',
+              fontWeight: 'bold',
+              textShadow: '1px 1px 3px #000' // 여기에 그림자를 추가
+            }}>
+              아직도 의료영상 CD를<br/>
+              수동으로 만드시나요?
+            </div>
+            <div style={{
+                position: 'absolute',
+                top: '50%',
+                left: '16%',
+                transform: 'translate(-50%, -50%)',
+                color: '#0C5A89',
+                fontSize: '25px',
+                textShadow: '1px 1px 3px #000' // 여기에도 그림자를 추가
+              }}>
+                플래니트의 의료영상 자동제작기를 통해<br/>
+                더 편리하고 확실하게 만들어보세요!
+            </div>
         </div>
     )
 }
