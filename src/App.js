556598ea55bed1568c7709f8753b4bb0d04f9d6a
Min Kim <68109760+mineruking@users.noreply.github.com>
2024-05-14 10:43:22 +0900

    V2.4
    Add Font, NavBar optimization

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react'; // React를 임포트합니다.
-import logo from './homeLogo.svg';
-import wallpaper from './images/wallpaper.jpg';
 import Main from './Pages/Main_page';
 import Notification from './Pages/Menu/Notification.jsx';
 import AboutUs from './Pages/Menu/AboutUs.jsx';
 import AboutProduct from './Pages/Menu/AboutProduct.jsx';
 import ContactUs from './Pages/Menu/ContactUs.jsx';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Nav, Navbar, Stack } from 'react-bootstrap'; // 필요한 컴포넌트들을 한 줄로 임포트합니다.
 import {Routes, Route, Navigate} from "react-router-dom";
-
+import Menu from './components/Navbar.jsx';
 function App() {
   return (
     <div>
+      <Menu />
       <Routes>
         <Route path='/' element={<Main></Main>}></Route>
         <Route path='/aboutus' element={<AboutUs></AboutUs>}></Route>
