75a05069790b62ba184955487dffa778cf02caa5
mineruking <kmin010287@gmail.com>
2024-07-10 11:21:48 +0900

    ...

--- src/Pages/Menu/AboutProduct.jsx
+++ src/Pages/Menu/AboutProduct.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../components/AboutProduct.css'; // 경로에 맞게 수정
+import d1 from '../../images/d1.png';
+import d2 from '../../images/d2.png';
+import d3 from '../../images/d3.png';
+import d4 from '../../images/d4.png';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -32,80 +36,22 @@ const AboutPage = () => {
           <Container>
             <Row className="gx-4 gy-4"> {/* gx-4와 gy-4 클래스를 사용하여 Col 간의 간격 조정 */}
               <Col className="AboutP-item">
-                <div className="AboutP-content">
-                  <h2><strong>기존 버닝 방식</strong></h2>
-                  <p>담당자가 CD/DVD를 직접 버닝해서 디스크 준비/버닝</p>
-                  <p>버닝된 디스크가 나오면 다시 디스크를 환자에게 전달하기 위해 검토</p>
-                  <p>디스크 프린터에 환자정보 라벨지 부착</p>
-                  <p>환자 배포 후 다음 작업 시작</p>
-                </div>
-              </Col>
-              <Col className="AboutP-item">
-                <div className="AboutP-content">
-                  <h2><strong>플래니트의 자동 제작 방식</strong></h2>
-                  <p>다수의 클라이언트가 동시에 Folder Watcher 프로그램을 통해 수시로 파일 전송</p>
-                  <p>장비에서 CD/DVD를 자동 구분하여 순차적으로 검증 후 인쇄</p>
-                </div>
+                <img alt="" src={d1} className="img1" />
               </Col>
             </Row>
             <Row className="gx-4 gy-4">
               <Col className="AboutP-item">
-                <div className="AboutP-content">
-                  <h2><strong>의료 영상 CD 자동 제작 과정</strong></h2>
-                  <p>서버에 도착한 순서대로 전송</p>
-                  <p>CD/DVD 자동 구분 제작</p>
-                  <p>100 디스크 버퍼 용량 (50개/50개 각각의 스태커 탑재)</p>
-                  <p>서버PC 제공 및 현황 조회/통계/과거 조회 기능 제공</p>
-                </div>
+                <img alt="" src={d2} className="img2" />
               </Col>
             </Row>
             <Row className="gx-4 gy-4">
               <Col className="AboutP-item">
-                <div className="AboutP-content">
-                  <h2><strong>스펙 비교표</strong></h2>
-                  <table className="table">
-                    <thead>
-                      <tr>
-                        <th>제품명</th>
-                        <th>Product specifications</th>
-                        <th>Weight</th>
-                        <th>Interface</th>
-                        <th>Data Transfer</th>
-                        <th>Speed</th>
-                        <th>Server PC</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td>PP-50II</td>
-                        <td>377 x 465 x 324mm</td>
-                        <td>24kg</td>
-                        <td>USB 2.0</td>
-                        <td>Client - Server - Printer</td>
-                        <td>15 Discs / hour</td>
-                        <td>8 Discs / hour</td>
-                      </tr>
-                      <tr>
-                        <td>PP-100III</td>
-                        <td>377 x 493 x 348mm</td>
-                        <td>25kg</td>
-                        <td>USB 3.0</td>
-                        <td>Client - Server - Printer</td>
-                        <td>30 Discs / hour</td>
-                        <td>15 Discs / hour</td>
-                      </tr>
-                      <tr>
-                        <td>PP-100N</td>
-                        <td>377 x 514 x 348mm</td>
-                        <td>25kg</td>
-                        <td>Ethernet Network</td>
-                        <td>Network - Printer</td>
-                        <td>30 Discs / hour</td>
-                        <td>15 Discs / hour</td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
+                <img alt="" src={d3} className="img3" />
+              </Col>
+            </Row>
+            <Row className="gx-4 gy-4">
+              <Col className="AboutP-item">
+                <img alt="" src={d4} className="img4" />
               </Col>
             </Row>
           </Container>
